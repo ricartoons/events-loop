26f266b6303985b04b596b65e7db67ce6e3e1356
Ricardo Justino <ricartoons@hotmail.com>
2020-01-28 09:47:59 -0300

    {eventos}[WIP] - cadastro de eventos

--- src/view/evento-cadastro/index.js
+++ src/view/evento-cadastro/index.js
@@ -7,22 +7,62 @@ import Navbar from '../../components/navbar/';
 import './evento-cadastro.css';
 
 export default function EventoCadastro() {
-  const [msgTipo, setMsgTipo] = useState('erro');
+
+  const [carregando, setCarregando] = useState();
+  const [msgTipo, setMsgTipo] = useState();
+  const [titulo, setTitulo] = useState();
+  const [tipo, setTipo] = useState();
+  const [detalhes, setDetalhes] = useState();
+  const [data, setData] = useState();
+  const [hora, setHora] = useState();
+  const [foto, setFoto] = useState();
+  const usuarioEmail = useSelector(state=>state.usuarioEmail);
+
+  const storage = firebase.storage();
+  const db = firebase.firestore();
+
+  function cadastrar() {
+    setMsgTipo(null);
+    setCarregando(true);
+
+    storage.ref(`imagens/${foto.name}`).put(foto).then(() => {
+          db.collection('eventos').add({
+            titulo: titulo,
+            tipo: tipo,
+            detalhes: detalhes,
+            data: data,
+            hora: hora,
+            usuario: usuarioEmail,
+            visualizacoes: 0,
+            foto: foto.name,
+            publico: 1,
+            criacao: new Date()
+          }).then(() => {
+            setMsgTipo('sucesso');
+            setCarregando(false);
+          }).catch(erro => {
+            setMsgTipo('erro');
+            setCarregando(false);
+          })
+        });
+  }
+
+
   return (
     <>
       <Navbar />
       <div className="col-12 mt-3">
         <div className="row">
-          <h3 className="mx-auto font-weight-bold">Novo evento</h3>
+          <h4 className="mx-auto font-weight-bold">Novo evento</h4>
         </div>
         <form>
           <div className="form-group">
             <label htmlFor="">Título</label>
-            <input type="text" className="form-control" />
+            <input type="text" className="form-control" onChange={(e)=>setTitulo(e.target.value)}/>
           </div>
           <div className="form-group">
             <label htmlFor="">Tipo do evento</label>
-            <select name="" id="">
+            <select name="" id="" className="form-control" onChange={(e)=> setTipo(e.target.value)}>
               <option disable selected value>Selecione um tipo</option>
               <option value="">Festa</option>
               <option value="">Teatro</option>
@@ -30,25 +70,30 @@ export default function EventoCadastro() {
               <option value="">Eventos</option>
             </select>
           </div>
-          <div className="form-group">
+          <div className="form-group"> 
             <label htmlFor="">Descrição do evento</label>
-            <textarea type="text" rows="3" className="form-control"></textarea>
+            <textarea type="text" rows="3" className="form-control" onChange={(e)=>setDetalhes(e.target.value)}></textarea>
           </div>
           <div className="form-group row">
             <div className="col-6">
               <label htmlFor="">Data</label>
-              <input type="date" className="form-control" />
+              <input type="date" className="form-control" onChange={(e)=>setData(e.target.value)}/>
             </div>
             <div className="col-6">
               <label htmlFor="">Hora</label>
-              <input type="time" className="form-control" />
+              <input type="time" className="form-control" onChange={(e)=>setHora(e.target.value)}/>
             </div>
           </div>
           <div className="form-group">
             <label htmlFor="">Upload da foto:</label>
-            <input type="file" className="form-control" />
+            <input type="file" className="form-control" onChange={(e)=>setFoto(e.target.files[0])}/>
           </div>
-          <button className="btn btn-lg btn-block mt-3 btn-cadastro">Publicar evento</button>
+          <div>
+            {
+              carregando ? <div class="spinner-border mx-auto" role="status"><span class="sr-only">Loading...</span></div>
+              : <button className="btn btn-lg btn-block mt-3 btn-cadastro" type="button" onClick={cadastrar}>Publicar evento</button>
+            }
+          </div>
         </form>
         {msgTipo === 'sucesso' &&
           <div className="msg-login text-center text-center">
